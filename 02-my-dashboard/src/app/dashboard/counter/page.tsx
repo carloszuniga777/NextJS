0f74c5cd63ad660620e46639cf478f65f607dd15
Carlos Zuniga <carloszuniga777@outlook.es>
2024-03-11 20:42:53 -0600

    creacion del contador

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,20 @@
+import { CarCounter } from "@/app/shopping-cart";
+
+
+//sniper metadata: mr
+export const metadata = {
+ title: 'Shopping Cart',
+ description: 'Un simple contador',
+};
+
+
 
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>Page Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CarCounter value={0}/>
     </div>
   );
 }
