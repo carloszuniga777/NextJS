623667cbac46f10e973fd47b11a0f0ce7482fedd
Carlos Zuniga <carloszuniga777@outlook.es>
2024-04-17 20:22:41 -0600

    primeros cambios boc v3

--- proyecto-tigo/src/middleware.ts
+++ proyecto-tigo/src/middleware.ts
@@ -1,5 +1,3 @@
-//poner el tipo ts
-
 //https://next-auth.js.org/configuration/nextjs
 
 export { default } from "next-auth/middleware" //Protege las url cuando un usuario no se ha authenticado, es decir, evita que el usuario ingrese a una pagina por medio de la url sin que se haya authenticado antes
