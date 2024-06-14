541483184b7c04891d10399a840e0305b4d827ba
Carlos Zuniga trabajo <carloszuniga777@outlook.es>
2024-06-14 10:56:47 -0600

    detalle producto

--- 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
@@ -1,9 +1,15 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+
+const seedProducts = initialData.products
 
 
 interface Props{
   params: {
-    id: string
+    id: Category
   }
 }
 
@@ -12,11 +18,27 @@ export default function({params}:Props) {
 
   const { id } = params
 
-  if(id === 'kids') notFound()
+
+  const products = seedProducts.filter(product => product.gender == id)
+
+  const labels: Record<Category, string> = {
+    'men':    'para hombres',
+    'women':  'para mujeres',
+    'kid':    'para niños',
+    'unisex': 'para todos'
+  }
+
+  
+  //if(id === 'kids') notFound()
 
     return (
-      <div>
-        <h1>Category Page {id}</h1>
-      </div>
+      <>
+        <Title
+          title={`Artículos de ${labels[id]}`}
+          subtitle='Todos los productos'
+          className='mb-2'
+        />
+        <ProductGrid products={products}/>
+    </>
     );
   }
--- 04-teslo-shop/src/app/(shop)/product/[slug]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/product/[slug]/page.tsx
@@ -1,9 +1,56 @@
-//snipett prc
+import { initialData } from "@/seed/seed";
+import notFound from "../not-found";
+import { titleFont } from "@/config/fonts";
 
-export default function ProductPage() {
+
+
+interface Props{
+  params:{
+    slug: string
+  }
+}
+
+//snipett prc
+export default function({ params }:Props) {
+
+  const { slug } = params
+  const product = initialData.products.find(product => product.slug === slug)
+
+  if(!product) notFound()
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      
+          {/**SlideShow */}
+          <div className="col-span-1 md:col-span-2">
+            Hola mundo
+          </div>
+
+          {/**Detalles */}
+          <div className="col-span-1 px-5">
+                <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+                  { product?.title }
+                </h1>
+                <p className="text-lg mb-5">${product?.price}</p>
+                 {/**Selector de tallas */}
+
+
+                  {/**Selector de cantidad */}
+
+
+                  {/**Boton */}
+                  <button className="btn-primary my-5">
+                    Agregar al carrito
+                  </button>
+
+
+                  {/**Descripcion*/}
+                  <h3 className="font-bold text-sm">Descripción</h3>
+                  <p className="font-light">
+                    { product?.description}
+                  </p>
+            
+          </div>
     </div>
   );
 }
