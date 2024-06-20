5c88c6db95e917c59dddca200bcd9e91cce1d771
Carlos Zuniga trabajo <carloszuniga777@outlook.es>
2024-06-20 09:26:12 -0600

    carrito de compra teslo - parte 2 y modificaciones en la conexion con posgress en proyecto tigo

--- proyecto-tigo/src/server/database.ts
+++ proyecto-tigo/src/server/database.ts
@@ -1,5 +1,24 @@
 import postgres from 'postgres'  //instalar: npm i postgres  | documentacion: https://www.npmjs.com/package/postgres
 
+
+const sql = postgres({
+    host: process.env.HOST_NAME,   
+    port: Number(process.env.PORT_NUMBER),  
+    database:process.env.DB_NAME,
+    username:process.env.USER_NAME, 
+    password:process.env.PGPASSWORD, 
+    idle_timeout:30,     //CERRAR CONEXION INACTIVA POR 30 SEGUNDOS
+    max: 500,              // Max number of connections
+    max_lifetime: null,  // Max lifetime in seconds (more info below) default: Note that max_lifetime = 60 * (30 + Math.random() * 30) by default. This resolves to an interval between 45 and 90 minutes to optimize for the benefits of prepared statements and working nicely with Linux's OOM killer.
+    connect_timeout: 120, // Connect timeout in seconds
+}) // will use psql environment variables
+
+export default sql
+
+
+/*
+import postgres from 'postgres'  //instalar: npm i postgres  | documentacion: https://www.npmjs.com/package/postgres
+
 export const sql = postgres(
     { host: process.env.HOST_NAME, 
       database: process.env.DB_NAME, 
@@ -8,6 +27,8 @@ export const sql = postgres(
       port:  process.env.PORT_NUMBER ? parseInt(process.env.PORT_NUMBER, 10) : 5432
     }
 );
+*/
+
 
 
 
